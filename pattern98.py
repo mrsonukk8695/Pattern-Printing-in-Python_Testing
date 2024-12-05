f1ca5f53635151fa5316a6946b1b88c2dd64a28c
mrsonukk8695 <mrsonukk8695@gmail.com>
2024-12-05 19:45:25 +0530

    98_updated

--- pattern98.py
+++ pattern98.py
@@ -14,3 +14,5 @@ for i in range(n, 0, -1):
     for j in range(1, n+1):
         print(i, end=' ') if i <= j else print(end='  ')
     print()
+  """Files must have clearly mentioned their working directory.
+  """
